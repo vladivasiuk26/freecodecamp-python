74290d5d2c66fca6b12adbdd39c90fd328f46fec
vladivasiuk26 <ivasiukvladyslav0@gmail.com>
2026-05-18 18:54:20 +0100

    Create freecodecamp.py

---
+++ freecodecamp.py
@@ -0,0 +1,7 @@
+name = str(input('Enter your name: '))
+surname = str(input('Enter your surname: '))
+full_name = name+' '+surname
+print(f"Your name is {full_name}")
+age = int(input('Enter your age: '))
+age = str(age)
+print(f'Your name is {name} and you are {age} years old')
